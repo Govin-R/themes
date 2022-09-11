31eb1e6b849ac1af4e2e3271a80294ab1b4783cb
Govin_R <rgovidaraj6092@gmail.com>
2022-09-11 20:29:58 +0530

    somechanges

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
+
 ## Themes for web 
+
+
+javascript:(function () {let script= document.createElement("script"); script.src="//cdn.jsdelivr.net/gh/Govin-R/themes/index.js"; document.body.appendChild(script))}();
--- index.js
+++ index.js
@@ -1,26 +1,25 @@
 
-let container = document.createElement("div");
-let onOff = document.createElement("input");
-let cirCle = document.createElement("span");
+document.addEventListener("DOMContentLoaded",()=>{
+		let stylesSheet = "<link rel='stylesheet' href='https://cdn.jsdelivr.net/gh/Govin-R/themes/styles.css' type='text/css' media='all'> "
+		document.head.outerHTML=stylesSheet;
+		let container = document.createElement("div");
+		let onOff = document.createElement("input");
+		let cirCle = document.createElement("span");
 
-container.setAttribute("id","input-container");
-onOff.setAttribute("type","checkbox");
-onOff.setAttribute("id","onOff");
-onOff.setAttribute("name","onOff");
-onOff.setAttribute("onChange","handleChange(this)");
-cirCle.setAttribute('id','cirCle');
+		container.setAttribute("id","input-container");
+		onOff.setAttribute("type","checkbox");
+		onOff.setAttribute("id","onOff");
+		onOff.setAttribute("name","onOff");
+		//onOff.setAttribute("onChange","handleChange(this)");
+		cirCle.setAttribute('id','cirCle');
 
-container.appendChild(onOff);
-container.appendChild(cirCle);
+		container.appendChild(onOff);
+		container.appendChild(cirCle);
 
-document.body.appendChild(container);
-
-function handleChange(checkbox){
-	if(checkbox.checked==true){
-		document.body.classList.toggle("theme-dark")
-			//console.log(document.body.classList())
-	}
-	else{
-		document.body.removeAttribute("class","theme-dark");	
-	}
-}
+		document.body.appendChild(container);
+	
+		let onoff= document.getElementById("onOff");
+		onOff.addEventListener("change",()=>{
+		document.body.classList.toggle("theme-dark");	
+	});
+})
